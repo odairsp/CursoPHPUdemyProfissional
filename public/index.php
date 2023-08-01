23b518351dfe6aae3a9c03ad40d1425c9b28fd67
odairsp <odair.sp@hotmail.com>
2023-08-01 09:34:00 -0300

    initial

--- public/index.php
+++ public/index.php
@@ -1,11 +1,19 @@
+<?php
+require "../vendor/autoload.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+        <?= TESTE ?>
+    </title>
 </head>
+
 <body>
     <h2>Index</h2>
 </body>
+
 </html>
