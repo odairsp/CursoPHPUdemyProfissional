dc0e72b9c10523a8fa75a8cbfd9a8bd433b063bf
odair <odair.sp@hotmail.com>
2023-08-07 17:33:37 -0300

    att

--- app/views/login.php
+++ app/views/login.php
@@ -1,1 +1,7 @@
 <h2>Login</h2>
+
+<div class="container mb-3">
+  <label for="" class="form-label">Email</label>
+  <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId">
+  <small id="emailHelpId" class="form-text text-muted">Help text</small>
+</div>
--- app/views/master.php
+++ app/views/master.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>
         <?= $title ?>
     </title>
@@ -16,9 +17,10 @@
             <?php require "partials/header.php" ?>
         </div>
     </header>
-    <div class="container">
+    <div class="container card">
         <?php require VIEWS . $view; ?>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 
 </html>
