655d1ab99bd40e43ebba76bbb32db837fd865619
odairsp <odair.sp@hotmail.com>
2023-08-08 15:08:07 -0300

    att

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -12,4 +12,26 @@ class Login
             'data' => ['title' => 'Login']
         ];
     }
+
+    public function store()
+    {
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        if (empty($email) || empty($password)) {
+            return redirect('/login');
+        }
+
+        $user = findBy('users', 'email', $email);
+        if (!$user) {
+            return redirect('/login');
+        }
+
+        if (!password_verify($password, $user->password)) {
+            return redirect('/login');
+        }
+
+        $_SESSION['logged'] = $user;
+        return redirect('/');
+    }
 }
